32bf96607bb49cbf1fbfbdddaec62512459709c1
beanbroker <88p7h21@gmail.com>
2019-03-17 21:23:14 +0900

    d

---
+++ src/main/kotlin/com/beanbroker/sample/domain/User.kt
@@ -0,0 +1,14 @@
+package com.beanbroker.sample.domain
+
+data class User(
+
+    var name : String = "",
+    var age : Int = 0
+
+
+
+) {
+    override fun toString(): String {
+        return "User(name='$name', age=$age)"
+    }
+}
--- src/test/kotlin/com/beanbroker/sample/SampleApplicationTests.kt
+++ src/test/kotlin/com/beanbroker/sample/SampleApplicationTests.kt
@@ -1,6 +1,7 @@
 package com.beanbroker.sample
 
 
+import com.beanbroker.sample.domain.User
 import com.beanbroker.sample.entity.UserEntity
 import com.beanbroker.sample.service.user.UserCreateService
 import com.beanbroker.sample.service.user.UserGetService
@@ -14,77 +15,75 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+//@RunWith(SpringRunner::class)
+//@SpringBootTest
 class SampleApplicationTests {
 
 
-	@Autowired
-	private lateinit var userGetService: UserGetService
+    @Autowired
+    private lateinit var userGetService: UserGetService
 
-	@Autowired
-	private lateinit var userCreateService: UserCreateService
+    @Autowired
+    private lateinit var userCreateService: UserCreateService
 
-	@Autowired
-	private lateinit var userUpdateService: UserUpdateService
+    @Autowired
+    private lateinit var userUpdateService: UserUpdateService
 
-	@Autowired
-	private lateinit var userTermService: UserTermService
-
-
-
-	@Test
-	fun queryDslTest() {
+    @Autowired
+    private lateinit var userTermService: UserTermService
 
 
-
-		userCreateService.saveUser(userEntity = UserEntity().apply {
-
-			this.name ="박기진"
-			this.age = 20
-			this.gender = "M"
-		})
+    @Test
+    fun queryDslTest() {
 
 
-		val userEntity = userGetService.getUserBySeq(42)
+        userCreateService.saveUser(userEntity = UserEntity().apply {
+
+            this.name = "박기진"
+            this.age = 20
+            this.gender = "M"
+        })
+
+
+        val userEntity = userGetService.getUserBySeq(42)
 
 
 
-		println("-------jpa geton--------")
-		userGetService.getUserByOriginJpa(42)
+        println("-------jpa geton--------")
+        userGetService.getUserByOriginJpa(42)
 
-		println("-------querydsl --------")
-		 userGetService.getUserBySeq(42)
+        println("-------querydsl --------")
+        userGetService.getUserBySeq(42)
 
-	}
-
-
-	@Test
-	fun auditTest() {
+    }
 
 
-		userUpdateService.updateUser(
+    @Test
+    fun auditTest() {
+
+
+        userUpdateService.updateUser(
 
             userGetService.getUserBySeq(42)?.apply {
 
-                age=100
+                age = 100
             }!!
 
 
-		)
+        )
 
-	}
-
-
-	@Test
-	fun termTest1() {
+    }
 
 
-		var test = userTermService.getUserTermByPredicator(
-			seq = 1,
-			firstTerm = "N",
-			secondTerm = "Y"
-		)
+    @Test
+    fun termTest1() {
+
+
+        var test = userTermService.getUserTermByPredicator(
+            seq = 1,
+            firstTerm = "N",
+            secondTerm = "Y"
+        )
 
 
         assertNotNull("데이터가 있는지?", test)
@@ -92,8 +91,89 @@ class SampleApplicationTests {
         assertEquals("두번째 약관이 맞는지?", 'Y', test.secondTerm)
         println(test.toString())
 
-	}
+    }
 
+
+    @Test
+    fun abd() {
+
+        // For loop
+        for (i in 0 until 10 step 3) {
+            if (i == 6) continue
+            println(i)
+        }
+
+
+        (1..10).forEach {
+            if (it == 6) return@forEach
+            println(it)
+        }
+
+    }
+
+
+    @Test
+    fun first() {
+
+        val users = listOf<User>(
+
+            User("pkj", 30),
+            User("beanbroker", 40),
+            User("A", 32),
+            User("B", 27),
+            User("C", 44),
+            User("D", 30)
+
+        )
+
+        val listOfOverThirtyFive = users.filter { it.age > 35 }
+        println(listOfOverThirtyFive.toString())
+
+
+        val mapOfOverThirtyFive = users.filter { it.age > 35}.map { it.name }
+
+        println(mapOfOverThirtyFive.toString())
+
+
+
+//        val isOlderThan50 = users.all { it.age > 50 }
+//
+//        println(isOlderThan50)
+//
+//        val isYoungerThan50 = users.all { it.age <40 }
+//        println(isYoungerThan50)
+//
+//        val isAnyOneOlderThan50 = users.any { it.age > 50 }
+//
+//        println(isAnyOneOlderThan50)
+//
+//        val isAnyOnYoungerThan50 = users.any { it.age <40 }
+//        println(isAnyOnYoungerThan50)
+
+
+//        val countWhoIsYoungerThan35 = users.count{it.age < 35}
+//        println(countWhoIsYoungerThan35)
+
+//        val someoneWhoseAgeis32 = users.find { it.age == 32 }
+//
+//        println(someoneWhoseAgeis32.toString())
+//
+//
+//        val someoneWhoseAgeis26 = users.find { it.age == 26 }
+//
+//
+//        println(someoneWhoseAgeis26.toString())
+
+
+
+
+        val userGroup = users.groupBy { it.age }.toList()
+
+        println(userGroup.toString())
+
+
+
+    }
 
 
 }
